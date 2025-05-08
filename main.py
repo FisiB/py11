7ab18a64ebce7cf31059d13bbb6a18c9f8840278
FisiB <fisi.budakova@gmail.com>
2025-05-08 20:47:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 file_path="C:/Users/Student/Desktop/py11/leximi.txt"
-# path=open(file_path,"r")
-# content=path.read()
-# print(content)
+path=open(file_path,"r")
+content=path.read()
+print(content)
 with open(file_path,"r")as file:
     content=file.read()
     print(content)
+
+with open(file_path,"r")as file:
+    line1=file.readline()
+    print(line1)
+
+with open(file_path, "w")as file:
+    file.write("Hello World")
+
+with open(file_path,"r")as file:
+    file.seek(1)
+    data=file.read()
+    print(data)
+
+    
