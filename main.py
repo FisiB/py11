7990db83c5e7e11b2c352eb33aa857a3ecaf9f40
FisiB <fisi.budakova@gmail.com>
2025-05-08 20:13:25 +0200

    f

---
+++ main.py
@@ -0,0 +1,7 @@
+file_path="C:/Users/Student/Desktop/py11/leximi.txt"
+# path=open(file_path,"r")
+# content=path.read()
+# print(content)
+with open(file_path,"r")as file:
+    content=file.read()
+    print(content)
