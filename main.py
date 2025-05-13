bc8db651985ab2d556ae2180757ea5a8615ed473
FisiB <fisi.budakova@gmail.com>
2025-05-13 20:05:38 +0200

    datetime

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime 
 file_path="C:/Users/Student/Desktop/py11/leximi.txt"
 path=open(file_path,"r")
 content=path.read()
@@ -18,4 +19,16 @@ with open(file_path,"r")as file:
     data=file.read()
     print(data)
 
+
+koha_aktuale = datetime.datetime.now()
+
+print(koha_aktuale)
+
+print("Year:",koha_aktuale.year)
+print("Month:",koha_aktuale.month)
+print("Day:",koha_aktuale.day)
+print("Hour:",koha_aktuale.hour)
+print("Minute:",koha_aktuale.minute)
+print("Second:",koha_aktuale.second)
+print("Mikroseconds:",koha_aktuale.microsecond)
     
