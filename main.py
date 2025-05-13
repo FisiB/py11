3dfe5b6c2cbbdfea64c9a2a164d96648d96ca114
FisiB <fisi.budakova@gmail.com>
2025-05-13 20:39:44 +0200

    challange

--- main.py
+++ main.py
@@ -31,4 +31,47 @@ print("Hour:",koha_aktuale.hour)
 print("Minute:",koha_aktuale.minute)
 print("Second:",koha_aktuale.second)
 print("Mikroseconds:",koha_aktuale.microsecond)
-    
+
+koha_aktuale1 = datetime.datetime.now().date()
+
+print(koha_aktuale1)
+
+koha_aktuale2 = datetime.datetime.now().time()
+
+print(koha_aktuale2)
+
+data=datetime.date(2024,5,12)
+print(data)
+time=datetime.time(12,23,44,23415)
+print(time)
+
+duration=datetime.timedelta(days=5 ,hours=3)
+
+new_Day=koha_aktuale + duration
+
+print(new_Day)
+
+bday=datetime.timedelta(days=7302)
+
+bday1=koha_aktuale1 - bday
+
+print(bday1)
+
+
+
+koha2 = datetime.timedelta(days=100)
+data2 = datetime.datetime.now()  # or any datetime object
+future_date = data2 + koha2
+future_date1 = data2 - koha2
+
+with open(file_path, "w") as file:
+    file.write("Koha: " + str(future_date))
+with open(file_path, "w") as file:
+    file.write("Koha: " + str(future_date1))
+
+
+
+
+with open(file_path,"r")as file:
+    content=file.read()
+    print(content)
